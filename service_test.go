c13dd9bbe43c77d9e32d98abad9e222ae17cfc68
Thomas Großbeck <grossbec@hm.edu>
2018-04-16 23:02:13 +0200

    Add service_test

---
+++ service_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"testing"
+	"net/http/httptest"
+	"net/http"
+)
+
+func Test(t *testing.T) {
+	//Generate Request
+	req, err := http.NewRequest("GET", "/query", nil);
+	if err != nil {
+		t.Fatal(err);
+	}
+
+	//ResponseRecorder
+	recorder := httptest.NewRecorder();
+	handler := http.HandlerFunc(sayhelloName);
+
+	handler.ServeHTTP(recorder, req);
+
+	//Check status
+	if recorder.Code != http.StatusOK {
+		t.Errorf("wrong status code: got %v want %v", recorder.Code, http.StatusOK);
+	}
+
+	//Check body
+	expected := "Hello world";
+	if recorder.Body.String() != expected {
+		t.Errorf("returned false body: got %v want %v", recorder.Body.String(), expected);
+	}
+
+}
